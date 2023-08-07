5886a9e0d7b549c2267ad0e25696c9e1effb9d93
johnalexsagwe <johnsagwe@gmail.com>
2023-08-07 04:34:39 -0700

    a function that splits a string into words. and a function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * create_array - Create an array of size `size` and assign character `c`.
- * @size: The size of the array.
- * @c: The character to assign to each element of the array.
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  *
- * Description: This function dynamically allocates an array of characters
- *of size `size` and initializes each element with the character `c`.
- *
- * Return: pointer to the new created array or NULL if memory allocation fails.
  */
 char *create_array(unsigned int size, char c)
-
 {
 	char *str;
 	unsigned int i;
@@ -20,6 +16,7 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
