fc7dc1dc09894ad7b04316d1ef72e9c2f7f2df72
johnalexsagwe <johnsagwe@gmail.com>
2023-09-04 13:05:01 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,10 +9,7 @@
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters it could read and print.
- *         If the file can not be opened or read, return 0.
- *         If filename is NULL, return 0.
- *         If write fails or does not write the expected bytes,return 0.
+ * Return: 1 or  0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
