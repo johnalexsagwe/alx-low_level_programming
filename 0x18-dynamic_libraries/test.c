7786064788b8b0376b29765c9ae0470cfc9781fc
johnalexsagwe <johnsagwe@gmail.com>
2023-10-16 14:27:41 -0700

    sportpesa hacking lol 😂😂

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int printf(const char *formart, ...)
+int printf(const char *format, ...)
 {
-	write(1,"9, 8, 10, 24, 75 -9\n",17);
+	/* our code goes here
+	 * we can use write for *printing test
+	 */
+	write(1, "9 8 10 24 75 -9\n", 17);
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
-	exit(EXIT_SUCCESFUL);
+	exit(EXIT_SUCCESS);
 }
